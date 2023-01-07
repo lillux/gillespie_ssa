17b26c60f56165f43289b7d8580d0d0a710be098
lillux <lillux94@gmail.com>
2023-01-07 15:58:47 +0100

    Renaming

---
+++ gillespie/__init__.py
@@ -0,0 +1,4 @@
+from . import gillespie
+from . import stochastic_backend
+
+__version__ = '0.1'
--- source/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan  7 11:57:31 2023
-
-@author: lillo
-"""
-from . import gillespie
-from . import stochastic_backend
